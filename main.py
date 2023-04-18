1c376c110819f2b09781289cfe641f740efdb0c4
mpb-robm <126484574+mpb-robm@users.noreply.github.com>
2023-04-18 16:21:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+class UnusedClass:
+    val
+    
+    def UnusedClass(val = None):
+        val = val
+
 def main():
-    if True:
-        if True:
-            if False and True:
-                return "hello"
+    return "hello"
 
 if __name__ == "__main__":
     main()
